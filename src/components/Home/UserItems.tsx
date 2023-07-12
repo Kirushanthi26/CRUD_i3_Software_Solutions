58f90c536116ae11fce9385d4861dff0f03d8d83
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-07-13 04:19:15 +0530

    delete button bug fixes

--- src/components/Home/UserItems.tsx
+++ src/components/Home/UserItems.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const UserItems:React.FC<{nameProp:string, telProp:number, emailProp:string, onRemoveCurdItem: () => void}> = (props) => {
   return (
-    <tr onClick={props.onRemoveCurdItem}>
+    <tr>
       <td>{props.nameProp}</td>
       <td>{props.telProp}</td>
       <td>{props.emailProp}</td>
       <td>
         <button>Edit</button>
-        <button>Delete</button>
+        <button onClick={props.onRemoveCurdItem}>Delete</button>
       </td>
     </tr>
   );
