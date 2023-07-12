80f60773c15dfe0d622d2df8874f8e7d601ba2b8
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-07-13 04:07:17 +0530

    form clear feature completed

--- src/components/Home/DetailsForm.tsx
+++ src/components/Home/DetailsForm.tsx
@@ -4,9 +4,9 @@ import { Curd } from "../../data_models/Crud.model";
 
 const DetailsForm:React.FC<{onAddUserDetailsList:(userDetail:Curd)=>void}> = (props) => {
     const [userDetails, setUserDetails] = useState<Curd>({
-        id:Math.random(),
+        id:+'',
         name:'',
-        tel:+'',
+        tel: parseInt(''),
         email:''
     })
 
@@ -15,31 +15,39 @@ const DetailsForm:React.FC<{onAddUserDetailsList:(userDetail:Curd)=>void}> = (pr
         setUserDetails((prev)=>{
             return {
                 ...prev,
-                [name]:value
+                id:Math.random(),
+                [name]:value,
             }
         })
+
     }
 
     const submitHandler = (e:React.FormEvent) => {
         e.preventDefault()
         props.onAddUserDetailsList(userDetails)
 
-       
+       console.log(userDetails)
+
+        setUserDetails({        
+            id:+'',
+            name:'',
+            tel:parseInt(''),
+            email:''})
     }
 
   return (
     <form className={style.curd} onSubmit={submitHandler}>
       <div className={style.control}>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" onChange={userDetailsHandler} />
+        <input type="text" name="name" id="name" onChange={userDetailsHandler} value={userDetails.name}/>
       </div>
       <div className={style.control}>
         <label htmlFor="tel">Telephone</label>
-        <input type="number" name="tel" id="tel" onChange={userDetailsHandler} />
+        <input type="number" name="tel" id="tel" onChange={userDetailsHandler} value={userDetails.tel}/>
       </div>
       <div className={style.control}>
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" onChange={userDetailsHandler}/>
+        <input type="email" name="email" id="email" onChange={userDetailsHandler} value={userDetails.email}/>
       </div>
       <div className={style.actions}>
         <button type="submit">ADD</button>
