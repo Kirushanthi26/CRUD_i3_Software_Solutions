08aa4d475a631d7556191147db182ba0dc9b3801
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-07-12 23:30:35 +0530

    header change

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import style from './Home.module.css'
 
 const Home:React.FC = (props) => {
   return (
-    <div>
+    <div className={style.home}>
      <h1>welcome to CURD!!!</h1> 
     </div>
   )
--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -5,7 +5,8 @@ import style from './MainHeader.module.css'
 const MainHeader:React.FC<{isAuthenticated:boolean, onLogout:()=>void}> = (props) => {
   return (
     <header className={style['main-header']}>
-      <h1>Login</h1>
+      {!props.isAuthenticated && <h1>Login</h1>}
+      {props.isAuthenticated && <h1>Home</h1>}
       <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout}/>
     </header>
   )
